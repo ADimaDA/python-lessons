7041ebf7e23a2d96529d19b5803dc50cf183f064
D1maDA <dimadiudnev@gmail.com>
2024-03-09 12:03:57 +0200

    lesson6

---
+++ p07/calculator.py
@@ -0,0 +1,13 @@
+print('Calculator')
+is_run = True
+
+while is_run:
+    print('Calculator')
+    print("You can: '+', '-', '*', '/', Exit: 'x'")
+
+    action = input('Action: ')
+
+    if action == 'x':
+        is_run = False
+
+
---
+++ p07/exercise2.py
@@ -0,0 +1,25 @@
+# list = [1, 2, 3, 4, 5, 6, 7, 77, 4]
+# list1[0] = 999
+# print(list1)
+# print(list[3])
+# list1.append(333)
+# print(list1)
+
+ # tuplex = (1, 3, 5)
+ # print(tuplex[0])
+
+ # dictionary = {'name': 'Ivan', 'age': 7}
+ # print(user['age'])
+
+
+tuplex = (10, 2 , 30, 90, 7, 50, 9, 44, 30, 1)
+summa = 0
+
+for x in  tuplex:
+    summa = summa + x
+
+
+
+
+
+
